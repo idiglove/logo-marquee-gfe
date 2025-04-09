5a3b003dab9c068af3ac0474a769499f8e09dc65
Faith M <faith.morante@yocale.com>
2025-04-09 16:05:40 -0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,77 @@
+<h1>Logo Marquee</h1>
+<!-- animate-loop -->
+<!-- transform-x-[100%] -->
+
+<div class="mx-auto w-full max-w-[1280px] overflow-hidden">
+	<div class="relative mx-auto flex h-16 gap-8" style="width: 200%">
+		<div class="animate-loop flex w-full justify-between gap-4">
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/wan-nain.svg"
+				alt="Wan Nain's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/robinwood.svg"
+				alt="Robinwood's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/swapdo.svg"
+				alt="Sawpdo's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/diamond.svg"
+				alt="Diamond's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/air-car.svg"
+				alt="Air Car's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/makro-hard.svg"
+				alt="Makro hard's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/wirang.svg"
+				alt="Wirang's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/bitterfly.svg"
+				alt="Bitterfly's logo"
+			/>
+		</div>
+
+		<div class="animate-loop flex w-full justify-between gap-4">
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/wan-nain.svg"
+				alt="Wan Nain's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/robinwood.svg"
+				alt="Robinwood's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/swapdo.svg"
+				alt="Sawpdo's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/diamond.svg"
+				alt="Diamond's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/air-car.svg"
+				alt="Air Car's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/makro-hard.svg"
+				alt="Makro hard's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/wirang.svg"
+				alt="Wirang's logo"
+			/>
+			<img
+				src="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/logo-marquee-section/starter/img/bitterfly.svg"
+				alt="Bitterfly's logo"
+			/>
+		</div>
+	</div>
+</div>
